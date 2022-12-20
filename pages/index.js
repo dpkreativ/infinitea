140acbf1be7f924289b55ec63907aa2a6dc2f895
Divine Orji <dpkreativ@gmail.com>
2022-12-20 06:22:14 +0100

    Get content from Unsplash

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import HomeLayout from '../components/layouts/HomeLayout';
 import Card from '../components/ui/Card';
 
+const BASE_URL = `https://api.unsplash.com/search/photos/`;
+
 export default function Home() {
+  // ------- States -------
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+
+  // ------- Functions -------
+  /**
+   * Fetch images from the Unsplash API and append the results to your `images` array
+   */
+  const fetchImages = async () => {
+    const response = await fetch(`${BASE_URL}?query=tea&page=${page}`, {
+      headers: {
+        Authorization: `Client-ID ${process.env.NEXT_PUBLIC_UNSPLASH}`,
+      },
+    });
+    const { results } = await response.json();
+    setImages((prev) => [...prev, ...results]);
+  };
+
+  /**
+   * useEffect to trigger the `fetchImages` function whenever `page` updates
+   */
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
+
+  // Render
   return (
     <>
       <Head>
